ca59127cc71cd444affdc632343ba32b1f7a5fc4
cherniaeva.a <cherniaeva.a@spectrumdata.ru>
2024-04-26 13:38:57 +0500

    init vue project

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <header>
+
+    <div class="wrapper">
+    </div>
+  </header>
+
+  <main>
+  </main>
+</template>
+
+<style scoped>
+
+
+</style>
